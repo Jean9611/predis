9c57b690bb6a458515b3dc1d16d60e2ae13d19e9
Jean9611 <jruizg96.11@gmail.com>
2017-07-13 08:20:10 -0600

    agregar los archivos de prueba

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+function mostrarMensaje(mensaje){
+	alert(mensaje);
+}
+
+function reDirect(url){
+	window.location.href = url;
+}
+
+function cargar_datos_req_fun_11(){
+	$("#lugar").val("Guatemala");
+  	$('#fecha').val("12/08/2017");
+  	$('#hora').val("6:00 p.m.");
+}
+
+function cargar_datos_req_fun_4_A1(){
+	$("#codigo").text("AYCCE-00001-2017");
+}
